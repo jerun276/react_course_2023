5a8ce75b78a4967b2ddd6f93850f5a90dc1bb460
jerun276 <jerunkingston@gmail.com>
2023-10-25 21:09:30 +0530

    usestate studied with count example

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import SubChildComponent from "./SubChildComponent";
 
+const initialStateValue = 45;
+
 function ChildComponent() {
+  const [counter, setCounter] = useState(initialStateValue);
+  console.log(counter);
+
+  function increaseCounter(){
+    setCounter(counter + 1);
+  }
+
+  function decreaseCounter(){
+    setCounter(counter - 1);
+  }
+
+  function resetCounter(){
+    setCounter(0);
+  }
+
+
   return (
     <div>
-      <button>Click</button>
+      <button onClick={increaseCounter}>Add</button>
+      <button onClick={decreaseCounter}>Decrease</button>
+      <button onClick={resetCounter}>Reset</button>
+   <p>Counter value is {counter}</p>
       <SubChildComponent />
     </div>
   );
